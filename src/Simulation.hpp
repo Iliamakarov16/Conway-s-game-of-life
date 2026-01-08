4b4036bb106992c51a202d84d5fdc459038ab1bd
Illia <Illiamakarov16@gmail.com>
2026-01-08 13:48:07 -0500

    test build

--- src/Simulation.hpp
+++ src/Simulation.hpp
@@ -4,12 +4,15 @@
 class Simulation{
 private:
     Grid grid;
+    Grid tempGrid;
 public:
-    Simulation(const int&  rows, const int&  cols, const int& cellSize) : grid(rows, cols, cellSize){};
+    Simulation(const int&  rows, const int&  cols, const int& cellSize) 
+    : grid(rows, cols, cellSize), tempGrid(rows, cols, cellSize){grid.fillRand();}
     
     void draw();
     void setValue(const int&  row, const int&  col, const bool& val);
     bool getValue(const int&  row, const int&  col) const;
-
+    
     int countLiveNeighbors(int row, int col);
+    void update();
 };
